c7e19afb32a63ba75f6824451d5ab3e216e9e490
Bardpoet <j884400@gmail.com>
2021-12-16 13:28:15 +0800

    first

---
+++ Assets/Scripts/useless/Managers/GameManager.cs
@@ -0,0 +1,245 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Photon.Pun;
+using System.Collections.Generic;
+using Tanks;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public int m_NumRoundsToWin = 5;        
+    public float m_StartDelay = 3f;         
+    public float m_EndDelay = 3f;           
+    public CameraControl m_CameraControl;   
+    public Text m_MessageText;              
+    public GameObject m_TankPrefab;         
+    public TankManager[] m_Tanks;
+
+    public static GameManager instance;
+    public GameObject localPlayer;
+    string gameVersion = "1";
+
+    private GameObject defaultSpawnPoint;
+
+    private int m_RoundNumber;              
+    private WaitForSeconds m_StartWait;     
+    private WaitForSeconds m_EndWait;       
+/*    private TankManager m_RoundWinner;
+    private TankManager m_GameWinner;       
+*/
+
+    const float k_MaxDepenetrationVelocity = float.PositiveInfinity;
+    #region Utility
+    public static List<GameObject> GetAllObjectsOfTypeInScene<T>()
+    {
+        var objectsInScene = new List<GameObject>();
+        foreach (var go in (GameObject[])Resources.FindObjectsOfTypeAll(typeof(GameObject)))
+        {
+            if (go.hideFlags == HideFlags.NotEditable ||
+                go.hideFlags == HideFlags.HideAndDontSave)
+                continue;
+            if (go.GetComponent<T>() != null)
+                objectsInScene.Add(go);
+        }
+        return objectsInScene;
+    }
+    #endregion
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogErrorFormat(gameObject,"Multiple instances of {0} is not allow",GetType().Name);
+            DestroyImmediate(gameObject);
+            return;
+        }
+        PhotonNetwork.AutomaticallySyncScene = true;
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+        
+        defaultSpawnPoint = new GameObject("Default SpawnPoint");
+        defaultSpawnPoint.transform.position = new Vector3(0, 0, 0);
+        defaultSpawnPoint.transform.SetParent(transform, false);
+    }
+
+    private void Start()
+    {
+        // This line fixes a change to the physics engine.
+        Physics.defaultMaxDepenetrationVelocity = k_MaxDepenetrationVelocity;
+        
+        m_StartWait = new WaitForSeconds(m_StartDelay);
+        m_EndWait = new WaitForSeconds(m_EndDelay);
+
+        SpawnAllTanks();
+        SetCameraTargets();
+
+        StartCoroutine(GameLoop());
+    }
+    
+    private Transform GetRandomSpawnPoint()
+    {
+    var spawnPoints = GetAllObjectsOfTypeInScene<SpawnPoint>();
+    return spawnPoints.Count == 0
+    ? defaultSpawnPoint.transform
+    : spawnPoints[Random.Range(0, spawnPoints.Count)].transform;
+    }
+
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (!PhotonNetwork.InRoom)
+        {
+            return;
+        }
+        var spawnPoint = GetRandomSpawnPoint();
+        localPlayer = PhotonNetwork.Instantiate("TankPlayer",spawnPoint.position,spawnPoint.rotation,0);
+        Debug.Log("Player Instance ID: " + localPlayer.GetInstanceID());
+    }
+
+    private void SpawnAllTanks()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].m_Instance =
+                Instantiate(m_TankPrefab, m_Tanks[i].m_SpawnPoint.position, m_Tanks[i].m_SpawnPoint.rotation) as GameObject;
+            m_Tanks[i].m_PlayerNumber = i + 1;
+            m_Tanks[i].Setup();
+        }
+    }
+
+
+    private void SetCameraTargets()
+    {
+        Transform[] targets = new Transform[m_Tanks.Length];
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+            targets[i] = m_Tanks[i].m_Instance.transform;
+        }
+
+        m_CameraControl.m_Targets = targets;
+    }
+
+
+    private IEnumerator GameLoop()
+    {
+        yield return StartCoroutine(RoundStarting());
+        yield return StartCoroutine(RoundPlaying());
+        yield return StartCoroutine(RoundEnding());
+
+/*        if (m_GameWinner != null)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            StartCoroutine(GameLoop());
+        }
+*/    }
+
+
+    private IEnumerator RoundStarting()
+    {
+        yield return m_StartWait;
+    }
+
+
+    private IEnumerator RoundPlaying()
+    {
+        yield return null;
+    }
+
+
+    private IEnumerator RoundEnding()
+    {
+        yield return m_EndWait;
+    }
+
+
+    private bool OneTankLeft()
+    {
+        int numTanksLeft = 0;
+
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            if (m_Tanks[i].m_Instance.activeSelf)
+                numTanksLeft++;
+        }
+
+        return numTanksLeft <= 1;
+    }
+
+/*
+    private TankManager GetRoundWinner()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            if (m_Tanks[i].m_Instance.activeSelf)
+                return m_Tanks[i];
+        }
+
+        return null;
+    }
+
+
+    private TankManager GetGameWinner()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            if (m_Tanks[i].m_Wins == m_NumRoundsToWin)
+                return m_Tanks[i];
+        }
+
+        return null;
+    }
+
+
+    private string EndMessage()
+    {
+        string message = "DRAW!";
+
+        if (m_RoundWinner != null)
+            message = m_RoundWinner.m_ColoredPlayerText + " WINS THE ROUND!";
+
+        message += "\n\n\n\n";
+
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            message += m_Tanks[i].m_ColoredPlayerText + ": " + m_Tanks[i].m_Wins + " WINS\n";
+        }
+
+        if (m_GameWinner != null)
+            message = m_GameWinner.m_ColoredPlayerText + " WINS THE GAME!";
+
+        return message;
+    }
+*/
+
+    private void ResetAllTanks()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].Reset();
+        }
+    }
+
+
+    private void EnableTankControl()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].EnableControl();
+        }
+    }
+
+
+    private void DisableTankControl()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].DisableControl();
+        }
+    }
+}
